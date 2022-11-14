d1d843f77b163f917b2239fbd4c8f1b01a0bd193
Matthew Mikhaiel <MattDFD@live.ca>
2022-11-14 16:38:27 +0100

    second commit

--- app/src/main/java/com/example/group4_comp304sec004_lab04/MainActivity.java
+++ app/src/main/java/com/example/group4_comp304sec004_lab04/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Hey can you see this?
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
