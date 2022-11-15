eed4f4232a84a097d7a926f31a440a384d9a509a
AqeishParvez <aqeishparvez@Aqeishs-Air.hitronhub.home>
2022-11-15 11:23:03 -0500

    updated patient class

--- app/src/main/java/com/example/group4_comp304sec004_lab04/Patient.java
+++ app/src/main/java/com/example/group4_comp304sec004_lab04/Patient.java
@@ -1,6 +1,7 @@
 package com.example.group4_comp304sec004_lab04;
 
 
+import android.arch.persistence.room.Entity;
 
 @Entity
 public class Patient {
@@ -11,5 +12,51 @@ public class Patient {
     private int nurseId;
     private int room;
 
+    public int getPatientId() {
+        return patientId;
+    }
 
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getNurseId() {
+        return nurseId;
+    }
+
+    public void setNurseId(int nurseId) {
+        this.nurseId = nurseId;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
 }
