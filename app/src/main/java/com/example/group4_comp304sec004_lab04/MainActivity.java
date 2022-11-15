8c97bfd8d110923cbaf3c6537c5544573bdecbec
Matthew Mikhaiel <MattDFD@live.ca>
2022-11-15 17:36:30 +0100

    added intnent

--- app/src/main/java/com/example/group4_comp304sec004_lab04/MainActivity.java
+++ app/src/main/java/com/example/group4_comp304sec004_lab04/MainActivity.java
@@ -2,17 +2,34 @@ package com.example.group4_comp304sec004_lab04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     //Hey can you see this?
     //I can see this
     //Awesome it worked!
+    Button enterbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(),login.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/group4_comp304sec004_lab04/login.java
@@ -0,0 +1,16 @@
+package com.example.group4_comp304sec004_lab04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
