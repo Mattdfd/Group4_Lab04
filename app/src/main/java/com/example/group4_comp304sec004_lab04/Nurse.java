98b71fbdd2ead74d3a17ea96f2ae438aa7804038
AqeishParvez <aqeishparvez@Aqeishs-Air.hitronhub.home>
2022-11-15 11:26:52 -0500

    Add Nurse Entity

---
+++ app/src/main/java/com/example/group4_comp304sec004_lab04/Nurse.java
@@ -0,0 +1,52 @@
+package com.example.group4_comp304sec004_lab04;
+
+import android.arch.persistence.room.Entity;
+
+@Entity
+public class Nurse {
+    private int nurseId;
+    private String firstname;
+    private String lastname;
+    private String department;
+    private String password;
+
+    public int getNurseId() {
+        return nurseId;
+    }
+
+    public void setNurseId(int nurseId) {
+        this.nurseId = nurseId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
