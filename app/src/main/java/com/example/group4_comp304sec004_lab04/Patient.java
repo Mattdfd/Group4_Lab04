4aac478192ffbe31a1f12f6b2427a347b62520de
AqeishParvez <aqeishparvez@Aqeishs-Air.hitronhub.home>
2022-11-15 11:15:00 -0500

    add dependencies and create patient class

---
+++ app/src/main/java/com/example/group4_comp304sec004_lab04/Patient.java
@@ -0,0 +1,13 @@
+package com.example.group4_comp304sec004_lab04;
+
+@Entity
+public class Patient {
+    private int patientId;
+    private String firstname;
+    private String lastname;
+    private String department;
+    private int nurseId;
+    private int room;
+
+
+}
